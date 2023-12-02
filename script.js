e3b012c33aeab037d39fce2d001a3653bcb77d69
thehsadowte <theshadowte@gmail.com>
2023-12-02 10:37:53 +0200

    initial

---
+++ script.js
@@ -0,0 +1,27 @@
+//^  Обʼєкти window and document
+
+// DOM - Обєкт для роботи з елементами на сторінці
+
+// window.alert
+// window.prompt
+// window.confirm
+
+//?  Застарілі / дlідівські методи пошуку елементів getElementById, getElementsByTagName, getElementsByClassName
+
+// const titleEl = document.getElementById('title');
+// console.log(titleEl)
+
+// const listLink = document.getElementsByClassName('list__link');
+// //& Сучасні методи пошуку елементів
+
+// document.querySelector(".container > h1");
+
+// повертає перший знайдений елемент
+// const listItem = document.querySelectorAll(".list__item");
+
+// console.log(listItem);
+
+// сервіс для генерації ДОМ-дерева
+
+// js-classes
+//*  навігація по DOM-дереву
