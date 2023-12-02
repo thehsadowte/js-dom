98d60edd8372495b09598f24c60ac3128f9dc023
thehsadowte <theshadowte@gmail.com>
2023-12-02 12:42:07 +0200

    1

--- script.js
+++ script.js
@@ -23,5 +23,109 @@
 
 // сервіс для генерації ДОМ-дерева
 
+// Dom-дерево складається із вузлів(node)
+
 // js-classes
-//*  навігація по DOM-дереву
+// //*  навігація по DOM-дереву
+// const listEl = document.querySelector(".list");
+
+// console.log(listEl.children); // колекція елементів конспект
+// img src and alt  .. changing an attributes
+
+//^  доступ до атрибутів
+//  el.hasAttribute('name') - перевіряє наявність атрибута
+// el.getAttribute('name') - отримує значення атрибута
+// el.setAttribute('name', 'value') - встановлює атрибут
+// console.dir(imgEl.setAttribute('alt', 'my image'));
+// el.removeAttribute('name') - видаляє атрибут
+// el.attributes - колекція всіх атрибутів елемента
+
+// titleEl.style.backgroundColor = 'red';
+// titleEl.style.fontSize = '30px';
+// inline styles
+
+// ? щоб одним рядком додати багато стилів
+// titleEl.style.cssText =
+//   "background-color: red; font-size: 30px; padding: 20px;";
+
+// we use these inline styles for dynamic changes.. slider for example
+
+//* методи classList (add, remove, toggle, contains)
+
+// titleEl.classList.add('newClass);
+
+//& create new element
+// const listItemEl = document.createElement("li");
+// поки що в домі його немає
+
+// listItemEl.classList.add("gallery__item");
+
+// методи додавання до Дом
+// append, prepend, before, after
+
+// listEl.append(listItemEl);
+
+//?  видалення елементів
+// removeChild, remove
+// listEl.removeChild(listEl.children[1]);
+// listEl.remove();
+
+// дорогі операції
+
+//оптимізація
+
+// task
+
+const pictures = [
+  {
+    width: 700,
+    height: 460,
+    url: "https://picsum.photos/id/0/700/460",
+    alt: "laptop",
+  },
+  {
+    width: 700,
+    height: 460,
+    url: "https://picsum.photos/id/1/700/460",
+    alt: "laptop",
+  },
+  {
+    width: 700,
+    height: 460,
+    url: "https://picsum.photos/id/10/700/460",
+    alt: "laptop",
+  },
+  {
+    width: 700,
+    height: 460,
+    url: "https://picsum.photos/id/100/700/460",
+    alt: "laptop",
+  },
+  {
+    width: 700,
+    height: 460,
+    url: "https://picsum.photos/id/1000/700/460",
+    alt: "laptop",
+  },
+];
+
+// функція для створення карточки makeGalleryCard(cardInfo);
+
+const galleryListEl = document.querySelector(".js-gallery");
+
+const makeGalleryCard = ({ width, height, url, alt } = {}) => {
+  const galleryItemEl = document.createElement("li");
+  galleryItemEl.classList.add("gallery-item");
+
+  const galleryImgEl = document.createElement("img");
+  galleryImgEl.src = url;
+  galleryImgEl.alt = alt;
+  galleryImgEl.width = width;
+  galleryImgEl.height = height;
+
+  return galleryItemEl;
+};
+
+const galleryItemsEl = pictures.map((el) => makeGalleryCard(el));
+
+galleryListEl.append(...galleryItemsEl);
